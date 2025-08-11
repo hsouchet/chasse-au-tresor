6fb4911f174282cc42856bbf27821a3541737ed8
hsouchet <107254491+hsouchet@users.noreply.github.com>
2025-08-11 16:19:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const indices = [
-    { lat: 45.123, lng: 5.456, texte: "En voiture Simone !" },
-    { lat: 45.123, lng: 5.456, texte: "Bien joué ! Dirige-toi maintenant vers le parc." },
+    { lat: 45.123, lng: 5.456, texte: "En voiture Simone, direction mc donals voreppe." },
+    { lat: 45.123, lng: 5.456, texte: "Sortie 10 tu prendras,." },
     { lat: 45.789, lng: 5.654, texte: "Tu es sur la bonne voie ! Rends-toi à la bibliothèque." },
 ];
 
